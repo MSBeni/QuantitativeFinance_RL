868077f09a6bfc1588fc468d22c099033b347f07
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-06-22 18:21:11 -0400

    Added simple example for using alpha vantage

---
+++ 2-DataGathering-AlphaVantage/alphavantage_TEST.py
@@ -0,0 +1,11 @@
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+
+key_path = "/home/i-sip_iot/s_vv/AlphaVantage.txt"
+
+ts = TimeSeries(key=open(key_path, 'r').read(), output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
+pprint(data.head(2))
