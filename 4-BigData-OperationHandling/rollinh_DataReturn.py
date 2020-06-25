80fe1b2cd0e2d6735f598d53d79d8c516d006b16
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-06-25 16:32:51 -0400

    Added DataPLotting section

---
+++ 4-BigData-OperationHandling/rollinh_DataReturn.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import os
+import errno
+import pandas_datareader.data as pdr
+import datetime
+
+
+def ensure_directory_exists(base_directory):
+    """
+    Makes a directory if it does not exist
+    """
+    try:
+        os.makedirs(base_directory)
+    except OSError as ex:
+        if ex.errno != errno.EEXIST:
+            raise ex
+
+
+tickers = ["MSFT", "AMZN", "AAPL", "CSCO", "IBM", "FB"]
+
+close_prices = pd.DataFrame()
+attempt = 0
+drop = []
+while len(tickers) != 0 and attempt <= 5:
+    tickers = [j for j in tickers if j not in drop]
+    for i in range(len(tickers)):
+        try:
+            temp = pdr.get_data_yahoo(tickers[i], datetime.date.today()-datetime.timedelta(3650), datetime.date.today())
+            temp.dropna(inplace=True)
+            close_prices[tickers[i]] = temp['Adj Close']
+            drop.append(tickers[i])
+
+        except:
+            print(tickers[i], ":Failed to fetch the data ... try again!!!")
+            continue
+
+close_prices.fillna(method='bfill', axis=0, inplace=True)
+
+daily_return = close_prices.pct_change()   # equal to: (close_prices/close_prices.shift(1)) - 1
+# print(daily_return)
+
+Rolling_DailyReturn_mean = daily_return.rolling(window=20, min_periods=1).mean()  # Simple moving average
+Rolling_DailyReturn_std = daily_return.rolling(window=20).std()
+
+ewm_DailyReturn_mean = daily_return.ewm(span=20, min_periods=20).mean()  # exponential moving average
+ewm_DailyReturn_std = daily_return.ewm(span=20, min_periods=20).std()
+
+print(Rolling_DailyReturn_mean)
+print(Rolling_DailyReturn_std)
+print(ewm_DailyReturn_mean)
+print(ewm_DailyReturn_std)
+
+# ensure_directory_exists('data')
+# Rolling_daily_return.to_csv('./data/Rolling_daily_return.csv')
+# Rolling_daily_return.to_json('./data/Rolling_daily_return.json')
