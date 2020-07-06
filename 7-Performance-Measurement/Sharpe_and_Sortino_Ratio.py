2528bb5eef9a7e033397cd44b94393e8561e17ed
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-07-06 15:46:17 -0400

    Added Sortino

--- 7-Performance-Measurement/Sharpe_and_Sortino_Ratio.py
+++ 7-Performance-Measurement/Sharpe_and_Sortino_Ratio.py
@@ -13,3 +13,16 @@ def sharpe(dataframe, risk_free_rate):
     df = dataframe.copy()
     Sharperate = (cagr(df) - risk_free_rate)/volatility(df)
     return Sharperate
+
+
+def sortino(df, rf):
+    """function to calculate sortino ratio ; rf is the risk free rate"""
+    df = df.copy()
+    df["daily_ret"] = df["Adj Close"].pct_change()
+    neg_vol = df[df["daily_ret"] < 0]["daily_ret"].std() * np.sqrt(252)
+    sr = (cagr(df) - rf)/neg_vol
+    return sr
+
+
+RFR = 0.022
+print(sortino(SnP, RFR))
