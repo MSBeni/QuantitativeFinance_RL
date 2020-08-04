cb5fe543b7ba765a584f78588e3de1d91375369c
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-08-04 15:04:33 -0400

    Added new FXCM API call edits

--- 10-Automated-Trading-System /FXCM/fxcm.py
+++ 10-Automated-Trading-System /FXCM/fxcm.py
@@ -15,29 +15,32 @@ data = con.get_candles(pair, period='m5', number=250)
 
 # print(data)
 
-# #streaming data
-# "for streaming data, we first need ti subscribe to a currency pair"
-# con.subscribe_market_data('EUR/USD')
-# con.get_last_price('EUR/USD')
-# con.get_prices('EUR/USD')
-# con.unsubscribe_market_data('EUR/USD')
-#
-# #trading account data
-# con.get_accounts().T
-#
-# con.get_open_positions().T
-# con.get_open_positions_summary().T
-#
+#streaming data
+"for streaming data, we first need ti subscribe to a currency pair"
+con.subscribe_market_data('EUR/USD')
+con.get_last_price('EUR/USD')
+con.get_prices('EUR/USD')
+con.unsubscribe_market_data('EUR/USD')
+
+# trading account data
+GetAccounts = con.get_accounts().T
+
+GetOpenPositions = con.get_open_positions().T
+GetOpenPositionsSummary = con.get_open_positions_summary().T
+
+GetClosedPositions = con.get_closed_positions(kind='dataframe').T
+print(GetClosedPositions)
+
 # con.get_closed_positions()
 #
 # con.get_orders()
-#
-# #orders
+
+#orders
 # con.create_market_buy_order('EUR/USD', 10)
 # con.create_market_buy_order('USD/CAD', 10)
 # con.create_market_sell_order('USD/CAD', 20)
 # con.create_market_sell_order('EUR/USD', 10)
-#
+
 # order = con.open_trade(symbol='USD/CAD', is_buy=False,
 #                        is_in_pips=True,
 #                        amount=10, time_in_force='GTC',
