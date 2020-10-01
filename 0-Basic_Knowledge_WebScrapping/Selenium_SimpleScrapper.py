9eb53e30c51c967cc9d53cff42b7270a1f9b3275
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-10-01 12:40:46 -0400

    Added the selenium simple example

--- 0-Basic_Knowledge_WebScrapping/Selenium_SimpleScrapper.py
+++ 0-Basic_Knowledge_WebScrapping/Selenium_SimpleScrapper.py
@@ -10,8 +10,18 @@ pathToChromeDriver = ""
 # please install and reference the latest version of chromedriver from the website bellow
 # http://chromedriver.chromium.org/downloads
 # browser = webdriver.Chrome(executable_path="/home/i-sip_iot/s_vv/chromedriver", options=options)
-driver = webdriver.Chrome(ChromeDriverManager().install())
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get(url)
 
-driver.get(url)
+# Getting the first elements
+# element = browser.find_element_by_xpath("html/*")
 
-driver.quit()
+# Getting all elements
+elements = browser.find_elements_by_xpath("html/*")
+
+for el in elements:
+    print(el.tag_name)
+    # print(el.text)
+    print(el.get_attribute('textContent'))
+
+browser.quit()
