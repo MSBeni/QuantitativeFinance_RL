765534cd83590ab6091a9fe8370369672c2fe968
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-11-21 14:18:21 -0500

    Added twitter data scrapper

--- 0-Basic_Knowledge_WebScrapping/Selenium/ScrappingTweets_time.py
+++ 0-Basic_Knowledge_WebScrapping/Selenium/ScrappingTweets_time.py
@@ -14,16 +14,46 @@ browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get(URL)
 
 element_ = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(
-    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/'
+    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[7]/'
                'div/div/article/div/div/div/div[2]')))
 
 print("The Tweet is: ")
 print(element_.get_attribute("textContent"))
 
-element_ = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(
-    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/'
+elements = browser.find_elements_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/'
+                                          'div/div/section/div/div/div[7]/div/div/article/div/div/div/div[2]/div[2]/'
+                                          'div[2]/div[3]/div[2]/div/div/div[2]/span/span')
+
+# Exporting the Time
+element_ = WebDriverWait(browser, 5).until(EC.visibility_of_element_located(
+    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[7]/'
                'div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/a')))
 
-print(element_.get_attribute("title"))
 print("The time of the Tweet is: ")
-print(element_.get_attribute("textContent"))
+print(element_.get_attribute("title"))
+
+
+
+# Exporting the retweets
+elements = WebDriverWait(browser, 5).until(EC.visibility_of_element_located(
+    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/'
+                                          'div/div/section/div/div/div[7]/div/div/article/div/div/div/div[2]/div[2]/'
+                                          'div[2]/div[3]/div[2]/div/div/div[2]/span/span')))
+print("The retweets of the Tweet is: ")
+print(elements.get_attribute("textContent"))
+
+
+# Exporting the replies
+elements = WebDriverWait(browser, 5).until(EC.visibility_of_element_located(
+    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[7]/'
+               'div/div/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div[1]/div/div/div[2]/span/span')))
+print("The replies on the Tweet is: ")
+print(elements.get_attribute("textContent"))
+
+
+# Exporting the likes
+elements = WebDriverWait(browser, 5).until(EC.visibility_of_element_located(
+    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[7]/'
+               'div/div/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div[3]/div/div/div[2]/span/span')))
+print("The Likes of the Tweet is: ")
+print(elements.get_attribute("textContent"))
